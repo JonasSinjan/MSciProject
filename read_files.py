8822e883732edb241066030b53afc7592555af8d
joanssinjan <einsteinisbae@gmail.com>
2019-10-15 21:01:37 +0100

    attempt to read csv

--- read_files.py
+++ read_files.py
@@ -4,14 +4,24 @@ import scipy as sp
 import glob
 
 #set this to the directory where the data is kept on your local machine
-path = r'C:\Users\jonas\MSci-Data\SoloA_2019-06-21--08-10-10_20'
-all_files = glob.glob(path + "/*.csv")
+# path = r'C:\Users\jonas\MSci-Data\SoloA_2019-06-21--08-10-10_20'
+# all_files = glob.glob(path + "/*.csv")
 
-li = []
+file_name = 'C:\Users\jonas\MSci-Data\SoloA_2019-06-21--08-10-10_20\SoloA_2019-06-19--16-19-29_1.csv'
 
-for filename in all_files:
-    df = pd.read_csv(filename, index_col = None, header=None, error_bad_lines=False, warn_bad_lines = False)
-    li.append(df)
+df = pd.read_csv(file_name, index_col = None, header=None, error_bad_lines=False, warn_bad_lines = False)
+df.head()
+cols = list(df.columns.values)
+print(cols)
+#print(df.iloc[1]
+#this concatenates all the csv files in one folder into one dataframe
+#NB: for the unpowered test where it is moved through the array - unsure if should be concatenated
+# li = []
 
-frame = pd.concat(li, axis=0, ignore_index=True)
+# for filename in all_files:
+#     df = pd.read_csv(filename, index_col = None, header=None, error_bad_lines=False, warn_bad_lines = False)
+#     li.append(df)
 
+# frame = pd.concat(li, ignore_index=True)
+
+#print(frame.tail())
