3031fe2e171d57c13c6e5262e210c05eee960c47
joanssinjan <einsteinisbae@gmail.com>
2019-10-16 18:48:29 +0100

    colums reorderd and merged with soloB files

--- read_files.py
+++ read_files.py
@@ -12,7 +12,7 @@ else:
     file_name = r'your_location'#insert here
 
 df = pd.read_csv(file_name, error_bad_lines=False, warn_bad_lines = False, skiprows = 351, sep=';')
-print(df.head())
+#print(df.head())
 cols = df.columns.tolist()
 #print(cols)
 
@@ -21,11 +21,29 @@ new_cols = cols[0:5] + cols[-16:-1] + [cols[-1]] + cols[13:17] + cols[9:13] + co
 #print(new_cols)
 
 df = df[new_cols]
+#print(df.head())
+
+#probes 10 - 10 are also missing - which means they are in the 'SoloB files'
+#here I am reading the corresponding soloB file and merging it with the above dataframe
+if jonas:
+    file_name = r'C:\Users\jonas\MSci-Data\SoloB_2019-06-21--08-09-10_20\SoloB_2019-06-19--16-17-38_1.csv'
+else:
+    file_name = r'your_location'#insert here
+
+df_B = pd.read_csv(file_name, error_bad_lines=False, warn_bad_lines = False, skiprows = 170, sep=';')
+
+#print(df_B.head())
+cols = df_B.columns.tolist()
+
+#this will reorder the columns into the correct order
+new_cols = cols[9:13] + cols[1:9] + cols[13:17]
+#print(new_cols)
+
+df_B = df_B[new_cols]
+
+df = pd.concat([df, df_B], axis = 1)
+
 print(df.head())
-
-#probes 9 - 10 are also missing - which means they are in the 'SoloB files'
-
-#so we could try to reorder the columns to get the probes in the right order and add in the missing probes from the solob files
 
 #the code below was an attempt at combining all the csv files into one dataframe - but this isn't working yet so you can ignore it
 
