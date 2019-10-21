d84766e7789b2a5d1f475e60496272039df74e4e
joanssinjan <einsteinisbae@gmail.com>
2019-10-22 00:13:19 +0100

    concat + avg attempts

--- day_one.py
+++ day_one.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from read_merge import soloA, soloB, concatenate, soloA_concat, soloB_concat
+from read_merge import soloA, soloB, read_files
 import pandas as pd
 
 def day_one():
@@ -7,41 +7,44 @@ def day_one():
     jonas = True
 
     if jonas:
-        file_path_A = r'C:\Users\jonas\MSci-Data\unpowered\SoloA_2019-06-21--08-10-10_20\SoloA_2019-06-21--08-10-10_1.csv'
-        file_path_B = r'C:\Users\jonas\MSci-Data\unpowered\SoloB_2019-06-21--08-09-10_20\SoloB_2019-06-21--08-09-10_1.csv'
-        path_A = r'C:\Users\jonas\MSci-Data\unpowered\SoloA_2019-06-21--08-10-10_20'
-        path_B = r'C:\Users\jonas\MSci-Data\unpowered\SoloB_2019-06-21--08-09-10_20'
+        file_path_A = r'C:\Users\jonas\MSci-Data\day_one\SoloA_2019-06-21--08-10-10_20\SoloA_2019-06-21--08-10-10_1.csv'
+        file_path_B = r'C:\Users\jonas\MSci-Data\day_one\SoloB_2019-06-21--08-09-10_20\SoloB_2019-06-21--08-09-10_1.csv'
+        path_A = r'C:\Users\jonas\MSci-Data\day_one\SoloA_2019-06-21--08-10-10_20'
+        path_B = r'C:\Users\jonas\MSci-Data\day_one\SoloB_2019-06-21--08-09-10_20'
     else:
         file_path_A = r'your_location'#insert here
         file_path_B = r'your_location'#insert here
 
+    soloA = True
+    df = read_files(path_A, soloA)
+    print(df.tail())
+    print(len(df))
     
-    # df_complete_A = soloA_concat(path_A)
-    # print(df_complete_A.head())
-    # print(df_complete_A.tail())
+    # df_A = soloA(file_path_A)
+    # df_B = soloB(file_path_B)
+
+    # df = concatenate(df_A, df_B)
+    # print(df.head())
+
+    #print(df[df['time']==1.00].index) #returns index 20 - proves that this data file is already sampled at 20Hz.
+
+    plot = False
+
+    if plot:
+        #plotting the raw probes results
+        plt.figure()
+        for col in df.columns.tolist()[1:5]:
+            plt.plot(df['time'], df[col], label=str(col))
+            
+        # for col in df.columns.tolist()[-4:0]:
+        #     plt.plot(df['time'], df[col], label=str(col))
+
+        plt.xlabel('Time (s)')
+        plt.ylabel('B (nT)')
+        #plt.legend()
+        plt.show()
+
     
-    df_A = soloA(file_path_A)
-    df_B = soloB(file_path_B)
-
-    df = concatenate(df_A, df_B)
-    print(df.head())
-
-    print(df[df['time']==1.00].index) #returns index 20 - proves that this data file is already sampled at 20Hz.
-
-    #plotting the raw probes results
-    plt.figure()
-    for col in df.columns.tolist()[1:5]:
-        plt.plot(df['time'], df[col], label=str(col))
-        
-    for col in df.columns.tolist()[-4:0]:
-        plt.plot(df['time'], df[col], label=str(col))
-
-    plt.xlabel('Time (s)')
-    plt.ylabel('B (nT)')
-    #plt.legend()
-    plt.show()
-
-    
-unpowered_analysis()
+day_one()
 
 
--- read_merge.py
+++ read_merge.py
@@ -4,94 +4,46 @@ import scipy as sp
 import glob
 import matplotlib.pyplot as plt
 
-def soloA_concat(path):
-    #file_path = r'C:\Users\jonas\MSci-Data\SoloA_2019-06-21--08-10-10_20'
+def read_files(path, soloA):
+    #path - location of folder to concat
+    #soloA - set to True if soloA, if soloB False
     all_files = glob.glob(path + "/*.csv")
-    li = []
-    i = 0
-    for filename in all_files:
-        if i == 0:
-            df = pd.read_csv(filename, error_bad_lines=False, warn_bad_lines = False, skiprows = 351, sep=';', dtype = np.float64)
-        else:
-            df = pd.read_csv(filename, header = None, error_bad_lines=False, warn_bad_lines = False, skiprows = 352, sep=';', dtype = np.float64)
+    li, length_var = [], 0
+    for filename in all_files:   
+        df = pd.read_csv(filename, error_bad_lines=False, warn_bad_lines = False, skiprows = 351, sep=';')
+        print(df['time'].iloc[0:round(len(df)/30)].mean())
+        #print('og = ', len(df))
+        df = df.groupby(np.arange(len(df))//30).mean()
+        print(df['time'].iloc[0])
+        #print('compressed = ', len(df))
         cols = df.columns.tolist()
-        new_cols = cols[0:5] + cols[-16:-1] + [cols[-1]] + cols[13:17] + cols[9:13] + cols[5:9] #this will reorder the columns into the correct order
+        if soloA:
+            new_cols = cols[0:5] + cols[-16:-1] + [cols[-1]] + cols[13:17] + cols[9:13] + cols[5:9] #this will reorder the columns into the correct order
+        else:
+            new_cols = cols[9:13] + cols[1:9] + cols[13:17]
         df = df[new_cols]
         li.append(df)
-        i += 1
-        if i == 2:
-            break
-    print(i)
-    df_A = pd.concat(li, ignore_index=True)
-    
-    return df_A
+        length_var += len(df)
+        #print('total len = ', length_var)
 
-def soloB_concat(path):
-    #file_path = r'C:\Users\jonas\MSci-Data\SoloA_2019-06-21--08-10-10_20'
-    all_files = glob.glob(path + "/*.csv")
-    li = []
-    i = 0
-    for filename in all_files:
-        if i == 0:
-            df = pd.read_csv(filename, error_bad_lines=False, warn_bad_lines = False, skiprows = 170, sep=';')
-        else:
-            df = pd.read_csv(filename, header = None, error_bad_lines=False, warn_bad_lines = False, skiprows = 170, sep=';')
-        cols = df.columns.tolist()
-        new_cols = cols[0:5] + cols[-16:-1] + [cols[-1]] + cols[13:17] + cols[9:13] + cols[5:9] #this will reorder the columns into the correct order
-        df = df[new_cols]
-        li.append(df)
-        i += 1
-    df_A = pd.concat(li, ignore_index=True)
+    df = pd.concat(li, ignore_index=True)
     
-    print(df_A.columns.tolist())
+    #print(1/df['time'].iloc[1]) #the new effective sampling rate
     
-    return df_A
+    return df
     
 def soloA(file_path):
     #skiprows is required as the read_csv function cannot read in the header of the csv files correctly for some reason - might need the header data later - need to fix
     df = pd.read_csv(file_path, error_bad_lines=False, warn_bad_lines = False, skiprows = 351, sep=';')
-    #print(df.head())
     cols = df.columns.tolist()
-    #this will reorder the columns into the correct order
-    new_cols = cols[0:5] + cols[-16:-1] + [cols[-1]] + cols[13:17] + cols[9:13] + cols[5:9]
+    new_cols = cols[0:5] + cols[-16:-1] + [cols[-1]] + cols[13:17] + cols[9:13] + cols[5:9] #reorder the columns into the correct order
     df = df[new_cols]
-
     return df
 
 def soloB(file_path):
     #skiprows is required as the read_csv function cannot read in the header of the csv files correctly for some reason - might need the header data later - need to fix
     df_B = pd.read_csv(file_path, error_bad_lines=False, warn_bad_lines = False, skiprows = 170, sep=';')
-
     cols = df_B.columns.tolist()
-    #reorder the columns into the correct order
-    new_cols = cols[9:13] + cols[1:9] + cols[13:17]
+    new_cols = cols[9:13] + cols[1:9] + cols[13:17]#reorder the columns into the correct order
     df_B = df_B[new_cols]
-
     return df_B
-
-def concatenate(a,b):
-    df = pd.concat([a, b], axis = 1)
-    #print(len(df))
-    #trying to average over every 50 rows, but unsure how to group and cant get it to work
-    # if df['time'].iloc[1] == 0.001:
-    #     #df = df.groupby(np.arange(round(len(df), -3))//50).mean()
-    #     df = df.groupby(df.index // 50).mean()
-    #     #df = df.groupby(50).mean()
-
-    #print(df.head())
-    return df
-
-    # collist = df.columns.tolist()
-    # df_new = pd.DataFrame(np.nan, index=range(len(df)), columns=collist)
-
-    # path = r'C:\Users\jonas\MSci-Data\SoloA_2019-06-21--08-10-10_20'
-    # all_files = glob.glob(path + "/*.csv")
-    #print(df.iloc[1]
-    #this concatenates all the csv files in one folder into one dataframe
-    #NB: for the unpowered test where it is moved through the array - unsure if should be concatenated
-    # li = []
-    # for filename in all_files:
-    #     df = pd.read_csv(filename, index_col = None, header=None, error_bad_lines=False, warn_bad_lines = False)
-    #     li.append(df)
-    # frame = pd.concat(li, ignore_index=True)
-    #print(frame.tail())
