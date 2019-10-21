3d06609d5efbcd88c8ededf213b8479df5b37a92
jonassinjan <33846422+JonasSinjan@users.noreply.github.com>
2019-10-21 16:36:39 +0100

    attempt to concat

--- read_merge.py
+++ read_merge.py
@@ -4,6 +4,49 @@ import scipy as sp
 import glob
 import matplotlib.pyplot as plt
 
+def soloA_concat(path):
+    #file_path = r'C:\Users\jonas\MSci-Data\SoloA_2019-06-21--08-10-10_20'
+    all_files = glob.glob(path + "/*.csv")
+    li = []
+    i = 0
+    for filename in all_files:
+        if i == 0:
+            df = pd.read_csv(filename, error_bad_lines=False, warn_bad_lines = False, skiprows = 351, sep=';', dtype = np.float64)
+        else:
+            df = pd.read_csv(filename, header = None, error_bad_lines=False, warn_bad_lines = False, skiprows = 352, sep=';', dtype = np.float64)
+        cols = df.columns.tolist()
+        new_cols = cols[0:5] + cols[-16:-1] + [cols[-1]] + cols[13:17] + cols[9:13] + cols[5:9] #this will reorder the columns into the correct order
+        df = df[new_cols]
+        li.append(df)
+        i += 1
+        if i == 2:
+            break
+    print(i)
+    df_A = pd.concat(li, ignore_index=True)
+    
+    return df_A
+
+def soloB_concat(path):
+    #file_path = r'C:\Users\jonas\MSci-Data\SoloA_2019-06-21--08-10-10_20'
+    all_files = glob.glob(path + "/*.csv")
+    li = []
+    i = 0
+    for filename in all_files:
+        if i == 0:
+            df = pd.read_csv(filename, error_bad_lines=False, warn_bad_lines = False, skiprows = 170, sep=';')
+        else:
+            df = pd.read_csv(filename, header = None, error_bad_lines=False, warn_bad_lines = False, skiprows = 170, sep=';')
+        cols = df.columns.tolist()
+        new_cols = cols[0:5] + cols[-16:-1] + [cols[-1]] + cols[13:17] + cols[9:13] + cols[5:9] #this will reorder the columns into the correct order
+        df = df[new_cols]
+        li.append(df)
+        i += 1
+    df_A = pd.concat(li, ignore_index=True)
+    
+    print(df_A.columns.tolist())
+    
+    return df_A
+    
 def soloA(file_path):
     #skiprows is required as the read_csv function cannot read in the header of the csv files correctly for some reason - might need the header data later - need to fix
     df = pd.read_csv(file_path, error_bad_lines=False, warn_bad_lines = False, skiprows = 351, sep=';')
@@ -40,13 +83,6 @@ def concatenate(a,b):
 
     # collist = df.columns.tolist()
     # df_new = pd.DataFrame(np.nan, index=range(len(df)), columns=collist)
-    
-    # index = 0
-    
-    # for index, row in df.iterrows()
-
-    #     index += 1
-    #the code below was an attempt at combining all the csv files into one dataframe - but this isn't working yet so you can ignore it
 
     # path = r'C:\Users\jonas\MSci-Data\SoloA_2019-06-21--08-10-10_20'
     # all_files = glob.glob(path + "/*.csv")
--- unpowered.py
+++ unpowered.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from read_merge import soloA, soloB, concatenate
+from read_merge import soloA, soloB, concatenate, soloA_concat, soloB_concat
 import pandas as pd
 
 def unpowered_analysis():
@@ -9,12 +9,18 @@ def unpowered_analysis():
     if jonas:
         file_path_A = r'C:\Users\jonas\MSci-Data\unpowered\SoloA_2019-06-21--08-10-10_20\SoloA_2019-06-21--08-10-10_1.csv'
         file_path_B = r'C:\Users\jonas\MSci-Data\unpowered\SoloB_2019-06-21--08-09-10_20\SoloB_2019-06-21--08-09-10_1.csv'
+        path_A = r'C:\Users\jonas\MSci-Data\unpowered\SoloA_2019-06-21--08-10-10_20'
+        path_B = r'C:\Users\jonas\MSci-Data\unpowered\SoloB_2019-06-21--08-09-10_20'
     else:
         file_path_A = r'your_location'#insert here
         file_path_B = r'your_location'#insert here
 
+    
+    # df_complete_A = soloA_concat(path_A)
+    # print(df_complete_A.head())
+    # print(df_complete_A.tail())
+    
     df_A = soloA(file_path_A)
-
     df_B = soloB(file_path_B)
 
     df = concatenate(df_A, df_B)
@@ -24,7 +30,10 @@ def unpowered_analysis():
 
     #plotting the raw probes results
     plt.figure()
-    for col in df.columns.tolist()[1:13]:
+    for col in df.columns.tolist()[1:5]:
+        plt.plot(df['time'], df[col], label=str(col))
+        
+    for col in df.columns.tolist()[-4:0]:
         plt.plot(df['time'], df[col], label=str(col))
 
     plt.xlabel('Time (s)')
