a5198eaa9f2c7c7433e3c7bebc0b87b332094a93
Katie Simkins <katie.simkins@outlook.com>
2019-10-22 12:50:56 +0100

    katie file paths added

--- day_two.py
+++ day_two.py
@@ -4,14 +4,14 @@ import pandas as pd
 
 def day_two():
 
-    jonas = True
+    jonas = False
 
     if jonas:
         file_path_A = r'C:\Users\jonas\MSci-Data\powered\SoloA_2019-06-24--08-14-46_9\SoloA_2019-06-24--08-14-46_1.csv' #the first couple of files in some of the folders are from earlier days
         file_path_B = r'C:\Users\jonas\MSci-Data\powered\SoloB_2019-06-24--08-14-24_20\SoloB_2019-06-24--08-14-24_1.csv'
     else:
-        file_path_A = r'your_location'#insert here
-        file_path_B = r'your_location'#insert here
+        file_path_A = os.path.expanduser("~/Documents/MsciProject/Data/SoloA_2019-06-24--08-14-46_9/SoloA_2019-06-24--08-14-46_1.csv")
+        file_path_B = os.path.expanduser("~/Documents/MsciProject/Data/SoloB_2019-06-24--08-14-24_20/SoloB_2019-06-24--08-14-24_1.csv")
 
     df_A = soloA(file_path_A)
     df_B = soloB(file_path_B)
