b52ae9b59142fc19c72fe238c0c9799990476b67
Katie Simkins <katie.simkins@outlook.com>
2019-10-25 12:28:02 +0100

    i dont know

--- day_one.py
+++ day_one.py
@@ -15,41 +15,57 @@ def day_one():
         path_A = r'C:\Users\jonas\MSci-Data\day_one\SoloA_2019-06-21--08-10-10_20'
         path_B = r'C:\Users\jonas\MSci-Data\day_one\SoloB_2019-06-21--08-09-10_20'
     else:
-        file_path_A = os.path.expanduser("~/Documents/MsciProject/Data/SoloA_2019-06-21--08-10-10_20/SoloA_2019-06-21--08-10-10_01.csv")
-        file_path_B = os.path.expanduser("~/Documents/MsciProject/Data/SoloB_2019-06-21--08-09-10_20/SoloB_2019-06-21--08-09-10_01.csv")
+        file_path_A = os.path.expanduser("~/Documents/MSciProject/Data/SoloA_2019-06-21--08-10-10_20/SoloA_2019-06-21--08-10-10_01.csv")
+        file_path_B = os.path.expanduser("~/Documents/MSciProject/Data/SoloB_2019-06-21--08-09-10_20/SoloB_2019-06-21--08-09-10_01.csv")
         path_A = os.path.expanduser("~/Documents/MSciProject/Data/SoloA_2019-06-21--08-10-10_20")
         path_B = os.path.expanduser("~/Documents/MSciProject/Data/SoloB_2019-06-21--08-09-10_20")
 
-
+    
+    
+    """
     align = False
     
     if align == True:
         
         df_A = soloA(file_path_A)
+        df_B = soloB(file_path_B)
         
-        max_index = []
-        print (df_A.iloc[0])
-        #for col in collist[1:]:
-        #    probe = df[col].tolist()
-        #    max_index.append(probe.index(max(probe, key = abs)))
-        #peak_index = max(max_index, key=max_index.count)
-        #peak_time = df['time'][peak_index]
-        #print(peak_time)
-
-
-
-    soloA_var = True
+        collist_A = ['Probe01_X','Probe01_Y','Probe01_Z','Probe01_||','Probe02_X','Probe02_Y']
+        max_index_A = []   #empty list for max abs values for each probe
+        #print (df_A.iloc[0])
+        for col in collist_A:
+            probe = df_A[col].tolist()
+            max_index_A.append(probe.index(max(probe, key = abs)))
+        peak_index_A = max(max_index_A, key=max_index_A.count)   #find the mode of the list - most commonly shared max point between probes in A
+        sample_rate = df_A['time'][1]-df_A['time'][0]
+        peak_time_A = peak_index_A*sample_rate
+        #peak_time_A = df_A['time'][peak_index_A]    #this is the time at which the largest peak occurs in A
+        print("A",peak_time_A)
+        
+        collist_B = ['Probe10_X','Probe10_Y','Probe10_Z','Probe10_||','Probe11_X','Probe11_Y']
+        max_index_B = []   #empty list for max abs values for each probe
+        for col in collist_B:
+            probe = df_B[col].tolist()
+            max_index_B.append(probe.index(max(probe, key = abs)))
+        peak_index_B = max(max_index_B, key=max_index_B.count)   #find the mode of the list - most commonly shared max point between probes in A
+        peak_time_B = peak_index_B*sample_rate    #this is the time at which the largest peak occurs in B
+        print("B",peak_time_B)
+    """
+    
+    soloA_var = False
 
     collist = ['time', 'Probe01_X'] #'Probe01_||'
     df = read_files(path_A, soloA_var, jonas, collist)
 
-    print(df)
-    print(len(df))
-    # df_A = soloA(file_path_A)
-    # df_B = soloB(file_path_B)
+    #print(df)
+    #print(len(df))
+    
+    
+    #df_A = soloA(file_path_A)
+    #df_B = soloB(file_path_B)
 
-    # df = concatenate(df_A, df_B)
-    # print(df.head())
+    #df = concatenate(df_A, df_B)
+    #print(df.head())
 
     #print(df[df['time']==1.00].index) #returns index 20 - proves that this data file is already sampled at 20Hz.
 
@@ -69,7 +85,7 @@ def day_one():
         plt.ylabel('B (nT)')
         plt.legend()
         plt.show()
-        
+
     
     #power spectral density plot
     x = df['Probe01_X'][:20000]
@@ -95,5 +111,8 @@ def day_one():
     
     
 day_one()
+
+
+
 
 
