3a6b4a8f62909f621574bbb4ece50dd0776c999e
jonassinjan <33846422+JonasSinjan@users.noreply.github.com>
2019-10-29 14:42:23 +0000

    removed nullformatter

--- day_one.py
+++ day_one.py
@@ -5,7 +5,7 @@ import os
 import numpy as np
 import scipy.signal as sps
 import time
-from matplotlib.ticker import NullFormatter
+
 
 
 def day_one(collist):
@@ -92,11 +92,7 @@ def day_one(collist):
     
     plt.subplot(224)
     plot_power(f_m, Pxx_m, probe_m)
-    # Format the minor tick labels of the y-axis into empty strings with
-    # `NullFormatter`, to avoid cumbering the axis with too many labels.
-    #plt.gca().yaxis.set_minor_formatter(NullFormatter())
-    # Adjust the subplot layout, because the logit one may take more space
-    # than usual, due to y-tick labels like "1 - 10^{-3}"
+    
     plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25, wspace=0.35)
     
     
