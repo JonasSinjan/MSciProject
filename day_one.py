22af917301d927c3f8bf5967cd8eb30367a1e725
joanssinjan <einsteinisbae@gmail.com>
2019-11-08 00:33:58 +0000

    auto sampling and power spec plot clean up

--- day_one.py
+++ day_one.py
@@ -13,13 +13,13 @@ from datetime import datetime
 import glob
 
 
-def day_one(all_files, collist, soloA_bool, num, start_dt, end_dt):
+def day_one(all_files, collist, soloA_bool, num, start_dt, end_dt, sampling_freq = None):
     #set this to the directory where the data is kept on your local computer
     if soloA_bool:
-        df = read_files(all_files, soloA_bool, jonas, collist, day=1, start_dt = start_dt, end_dt = end_dt)
+        df = read_files(all_files, soloA_bool, jonas, sampling_freq, collist, day=1, start_dt = start_dt, end_dt = end_dt)
         rotate_mat = rotate_21(soloA_bool)[num-1]
     else:
-        df = read_files(all_files, soloA_bool, jonas, collist, day=1, start_dt = start_dt, end_dt = end_dt)
+        df = read_files(all_files, soloA_bool, jonas, sampling_freq, collist, day=1, start_dt = start_dt, end_dt = end_dt)
         rotate_mat = rotate_21(soloA_bool)[num-9]
     df.iloc[:,0:3] = np.matmul(rotate_mat, df.iloc[:,0:3].values.T).T
     print(len(df))
@@ -40,25 +40,25 @@ def day_one(all_files, collist, soloA_bool, num, start_dt, end_dt):
             print(df2[col].abs().idxmax())
         plt.xlabel('Time (s)')
         plt.ylabel('B (nT)')
-        plt.title(f'Probe {num}, {start_dt.date()}')
+        plt.title(f'Probe {num} @ {sampling_freq}Hz, {start_dt.date()}')
         plt.legend(loc="best")
         plt.show()
 
-    fs = 100
+    fs = 50
 
     powerspecplot(df, fs, collist)
     #print(len(df2))
     #spectogram
     """
     x = df2[collist[1]]
-    #fs = 200 # sampling rate
+    fs = 20 # sampling rate
     #f, Pxx = sps.periodogram(x,fs)
     f, t, Sxx = sps.spectrogram(x,fs)#,nperseg=700)
     plt.figure()
     plt.pcolormesh(t, f, Sxx,vmin = 0.,vmax = 0.1)
     plt.ylabel('Frequency [Hz]')
     plt.xlabel('Time [sec]')
-    plt.title('Spectogram')
+    plt.title(f'Spectogram {collist[1]}')
     plt.clim()
     fig = plt.gcf()
     plt.colorbar()  
@@ -89,16 +89,19 @@ if __name__ == "__main__":
         path_fol_A = os.path.expanduser("~/Documents/MSciProject/Data/day_one/A")
         path_fol_B = os.path.expanduser("~/Documents/MSciProject/Data/day_one/B")
 
-    num = 6
-    soloA_bool = True
+    num = 7
+    if num < 9:
+        soloA_bool = True
+    else:
+        soloA_bool = False
     if num <10:
         num_str = f'0{num}'
     else: 
         num_str = num
     collist = ['time', f'Probe{num_str}_X', f'Probe{num_str}_Y', f'Probe{num_str}_Z']
 
-    start_dt = datetime(2019,6,21,9,37)# this is the start of the time we want to look at, #datetime(2019,6,21,10,57,50)
-    end_dt = datetime(2019,6,21,9,40)# this is the end
+    start_dt = datetime(2019,6,21,11,28)# this is the start of the time we want to look at, #datetime(2019,6,21,10,57,50)
+    end_dt = datetime(2019,6,21,11,30)# this is the end
 
     day = 1
     start_csv, end_csv = which_csvs(soloA_bool, day ,start_dt, end_dt) #this function (in processing.py) finds the number at the end of the csv files we want
@@ -118,7 +121,7 @@ if __name__ == "__main__":
             else:
                 all_files[index] = path_fol_B + os.path.expanduser(f'/SoloB_2019-06-21--08-09-10_{i}.csv') #need to change path_fol_B to the path where your B folder is
     #print(all_files)
-    day_one(all_files, collist, soloA_bool, num, start_dt, end_dt) #pass through the list containing the file paths
+    day_one(all_files, collist, soloA_bool, num, start_dt, end_dt, sampling_freq = 50) #pass through the list containing the file paths
 
 
 
