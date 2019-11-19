d722a13bbf0d4484285512b9f369e6eac13d6d51
Katie Simkins <katie.simkins@outlook.com>
2019-11-19 14:42:24 +0000

    resampling

--- current.py
+++ current.py
@@ -27,13 +27,28 @@ def current(jonas):
     
     df =  pd.read_excel(filename)
     df.set_index(['EGSE Time'], inplace = True)
-    #df = df.resample(f'{2}s').mean()
+    df = df.resample(f'{10}s').mean()
     print (df.head())
+    
     
     plot = True
     if plot:
+        i=1
         plt.figure()
-        for col in df.columns[1:]:
+        for col in df.columns:
+            """
+            plt.figure(i)
+            i+=1
+            df[col] -= np.average(df[col])
+            step = np.hstack((np.ones(len(df[col])), -1*np.ones(len(df[col]))))
+            dary_step = np.convolve(df[col], step, mode='valid')
+            # get the peak of the convolution, its index
+            step_indx = np.argmax(dary_step)  # yes, cleaner than np.where(dary_step == dary_step.max())[0][0]
+            # plots
+            plt.plot(df[col])
+            plt.plot(dary_step/10)
+            plt.plot((step_indx, step_indx), (dary_step[step_indx]/10, 0), 'r')
+            """
             plt.plot(df.index.time, df[col], label=str(col))
             plt.legend(loc='best')
             plt.xlabel('Time [H:M:S]')
