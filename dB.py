b0e1594efe152dd12314dd56444a3bb9e1d6ddb3
Katie Simkins <katie.simkins@outlook.com>
2019-12-11 19:33:34 +0000

    db

--- dB.py
+++ dB.py
@@ -173,9 +173,9 @@ def dB(peak_datetimes, instrument, current_dif, jonas, probe_list, plot=False): 
     return vect_dict
 
 if __name__ == "__main__":
-    jonas = True
+    jonas = False
     dict_current = current_peaks(jonas, plot=False)
-    instrument = 'SPICE'
+    instrument = 'METIS'
     peak_datetimes = dict_current.get(f'{instrument} Current [A]')
     print(peak_datetimes[0], peak_datetimes[-1])
     current_dif = dict_current.get(f'{instrument} Current [A] dI')
