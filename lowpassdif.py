3775eefbf277fd8d2a92bd6f1e13baa14c75fd83
joanssinjan <einsteinisbae@gmail.com>
2020-01-01 16:42:40 +0000

    df for each probe & mean % diff from no lowpass

--- lowpassdif.py
+++ lowpassdif.py
@@ -13,16 +13,23 @@ for idx, instrument in enumerate(instru_list):
     lp_false.set_index('Probe', inplace = True)
     lp_true.set_index('Probe', inplace = True)
 
-    print(lp_false.head())
-    print(lp_true.head())
+    #print(lp_false.head())
+    #print(lp_true.head())
 
     lp_diff = lp_false.subtract(lp_true)
     lp_percent = (lp_diff/lp_false)*100
-
+    print(lp_percent.iloc[0])
     df_percent_arr[idx] = lp_percent
 
-tmp = 0
-for df in df_percent_arr:
-    for i in range(12):
-        tmp += df.iloc[i]
+#make one dataframe with all the information
+df_concat = pd.concat(df_percent_arr)
 
+#filter the overall df for the desired probe
+probe_percent_dif_lowpass_dict = {}
+for i in range(1,13):
+    df_tmp = df_concat[df_concat.index.isin([i])]
+    df_tmp['Instrument'] = instru_list
+    df_tmp.loc['mean'] = df_tmp.mean()
+    probe_percent_dif_lowpass_dict[f'Probe {i}'] = df_tmp
+    
+print(probe_percent_dif_lowpass_dict)
