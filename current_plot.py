988981cfb8fc692782a6996df862e161f3a0d0f0
jonassinjan <33846422+JonasSinjan@users.noreply.github.com>
2020-01-29 15:20:58 +0100

    eui attempt

--- current_plot.py
+++ current_plot.py
@@ -6,13 +6,13 @@ import numpy as np
 filename = r'C:\Users\jonas\MSci-Data\LCL_data\Day 2 Payload LCL Current Profiles.xlsx'
 df =  pd.read_excel(filename)
 df.set_index(['EGSE Time'], inplace = True)
-inst = 'METIS'
+inst = 'EUI'
 #df = df.resample(f'{1}s').mean()
 #eui 9:24 - 10:10
 #phi 8:03 - 8:40
 #metis 10:10 - 10:57
 df = df[f'{inst} Current [A]']
-df = df.between_time('10:34:30', '10:55')
+df = df.between_time('9:40:30', '10:10')
 print(df.head())
 plt.figure()
 plt.plot(df.index.time, df)
@@ -38,11 +38,28 @@ tot_std = np.sqrt(bot_std**2 + top_std**2)
 dif = top_avg - bot_avg
 print(dif, tot_std)
 
+"""
+# -------METIS-----------#
 #metis current variation during scientific operation is 0.1286 +/- 0.0003 A
 
 #metis only significant signal in Y at probe 10: 0.29nT/A
 var = 0.29*dif
 #when multiplying together, add the fractional errors in quadrature
 err = np.sqrt((0.13/0.29)**2 + (tot_std/dif)**2) #fractional error in variation
-print(var, '+/-', err*var) #in nT
-#metis B var at MAG-OBS: 0.034 +/- 0
+print(round(var, 4), '+/-', round(err*var,4), 'nT') #in nT
+#metis B var at MAG-OBS: 37 +/- 17 pT
+"""
+
+# -------EUI----------#
+dif = 0.8 #rough estimate as variation not as constant as with metis
+tot_std = 0.1
+tot_grad = np.sqrt(0.71**2 + 1.02**2)
+
+print(tot_grad, 'nT/A')
+
+grad_err = np.sqrt(((0.71/tot_grad)**2)*(0.19**2) + ((1.02/tot_grad)**2)*(0.19**2))
+
+var = dif * tot_grad
+
+var_err = np.sqrt((grad_err/var)**2 + (tot_std/dif)**2)
+print(1000*round(var, 5), '+/-', 1000*round(var_err*var,5), 'pT') #in nT
