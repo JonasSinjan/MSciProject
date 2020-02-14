8f2fe9c7ed31dd245ea750abe5959f1f7e9f49a7
joanssinjan <einsteinisbae@gmail.com>
2020-02-14 17:27:36 +0000

    added day 1 metis times variation

--- variation.py
+++ variation.py
@@ -48,38 +48,22 @@ def calc_var(windows, day, inst):
     var_mag_Z = []
     var_mag_Z_err = []
 
-    if inst == 'EUI':
-        df_current_list.append(df_current.between_time('9:49:00', '9:54:40'))
-        df_current_list.append(df_current.between_time('9:54:30', '10:03:20'))
-        df_mag_list.append(df_mag.between_time('9:49:00', '9:54:40'))
-        df_mag_list.append(df_mag.between_time('9:54:30', '10:03:20'))
-    elif inst == 'METIS':
-        df_current_list.append(df_current.between_time('10:35:30', '10:55'))
-        df_mag_list.append(df_mag.between_time('10:35:30', '10:55'))
-
-
-    for df in df_current_list:
-        plt.figure()
-        plt.plot(df.index.time, df)
-        plt.xlabel('Time [H:M:S]')
-        plt.ylabel('Current [A]')
-        plt.title(f'{inst} CURRENT PROFILE')
-        plt.show()
-
-        #calculate amplition of variation
-        mean_val = df.mean()
-        df_top = df[df > mean_val]
-        df_bot = df[df < mean_val]
-        top_avg = df_top.mean()
-        top_std = df_top.std()/np.sqrt(len(df_top))
-        bot_avg = df_bot.mean()
-        bot_std = df_bot.std()/np.sqrt(len(df_bot))
-        tot_std = np.sqrt(bot_std**2 + top_std**2)
-        dif = top_avg - bot_avg
-
-        var_current.append(dif)
-        var_current_err.append(tot_std)
-        print(var_current_err)
+    if day == 2:
+        if inst == 'EUI':
+            df_current_list.append(df_current.between_time('9:49:00', '9:54:40'))
+            df_current_list.append(df_current.between_time('9:54:30', '10:03:20'))
+            df_mag_list.append(df_mag.between_time('9:49:00', '9:54:40'))
+            df_mag_list.append(df_mag.between_time('9:54:30', '10:03:20'))
+        elif inst == 'METIS':
+            df_current_list.append(df_current.between_time('10:35:30', '10:55'))
+            df_mag_list.append(df_mag.between_time('10:35:30', '10:55'))
+            
+    elif day == 1:
+        if inst == 'EUI':
+            pass
+        elif inst == 'METIS':
+            df_current_list.append(df_current.between_time('12:07', '14:17'))
+            df_mag_list.append(df_mag.between_time('12:07', '14:17'))
 
         #print("current variation (A)", dif,"+/-" , tot_std)
     #dont think this should be used for MAG - only really apt for the metis current variation as so consistent.
@@ -119,7 +103,7 @@ def calc_var(windows, day, inst):
 
 
     if windows: #jonas put ur equivalent filepath here 
-        filename_MFSA = f'C:\\Users\\jonas\\MSci-Code\\MsciProject\\Results\\Gradient_dicts\\Day_{day}\\1hz_noorigin\\cur\\{inst}_vect_dict_NOORIGIN_Day2_curve_fit.csv'
+        filename_MFSA = f'C:\\Users\\jonas\\MSci-Code\\MsciProject\\Results\\Gradient_dicts\\Day_{day}\\1hz_noorigin\\cur\\{inst}_vect_dict_NOORIGIN_Day{day}_curve_fit.csv'
     else: 
         filename_MFSA = os.path.expanduser(f"~/Documents/MSciProject/NewCode/Gradient_dicts/Day_{day}/cur/1Hz_NoOrigin/{inst}_vect_dict_NOORIGIN.csv")
             
@@ -161,7 +145,7 @@ def calc_var(windows, day, inst):
         B_variation['current'] = [var_current[i],var_current[i],var_current[i],var_current_err[i],var_current_err[i],var_current_err[i]]
         
 
-        w = csv.writer(open(f"{inst}_var{i+1}_B_variation_estimated.csv", "w"))
+        w = csv.writer(open(f"{inst}_var{i+1}_B_variation_estimated_day{day}.csv", "w"))
         w.writerow(["Probe","var_X", "var_Y", "var_Z","var_X_err", "var_Y_err", "var_Z_err"])
         for key, val in B_variation.items():
             w.writerow([key,val[0],val[1],val[2],val[3],val[4],val[5]])
@@ -169,7 +153,7 @@ def calc_var(windows, day, inst):
 
 if __name__ == "__main__":
     windows = True
-    day = 2
+    day = 1
     inst = 'METIS'
 
     calc_var(windows, day, inst)
