3ffa4d269bae6a7725a63471f72b17c4c61a43d6
Katie Simkins <katie.simkins@outlook.com>
2020-02-14 11:58:56 +0000

    cubic graph

--- plot_variation.py
+++ plot_variation.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 import csv
 import os
+import scipy.optimize as spo
 
 def plot_b_var_est(file_path, inst, plot = False):
     df = pd.read_csv(file_path)
@@ -19,19 +20,30 @@ def plot_b_var_est(file_path, inst, plot = False):
     print(df.tail())
 
     df['dist'] = [3.1622776601683795, 3.7721081638786553, 3.12, 3.4705619141574178, 3.1622776601683795, 1.8880677953929514, 1.12, 2.3976655313033137, 2.32800880582527, 4.154843559028427, 4.1089475538147235]
-    
+
     if plot:
         """
         ax = df.plot.bar(x='Probe', y = 'B_tot', yerr = 'B_tot_err', rot = 0, legend = False, title=f'{inst}_B_var')
         ax.set_ylabel("B_var [nT]")
         plt.show()
-        """
-        #plt.plot(dist,)
+
         ax = df.plot.bar(x='dist', y = 'B_tot', yerr = 'B_tot_err', rot = 0, legend = False, title=f'{inst}_B_var')
         ax.set_ylabel("B_var [nT]")
         plt.show()
+        """
+
+        def cubic(x,a,b):
+                return a*(x**(-3)) + b
         
-        
+        params,cov = spo.curve_fit(cubic, df['dist'], df['B_tot'], sigma = df['B_tot_err'], absolute_sigma = True)
+        perr = np.sqrt(np.diag(cov))
+
+        plt.figure()
+        plt.scatter(df['dist'],df['B_tot'])
+        plt.errorbar(df['dist'],df['B_tot'],yerr=df['B_tot_err'], linestyle="None")
+        plt.plot(df['dist'], params[0]*(df['dist'])**(-3) + params[1], 'b-',label='_nolegend_')
+        plt.show()
+
 
 if __name__ == "__main__":
     windows = False
