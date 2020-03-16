673928308e1379d7aa66de208d4ee71e62f46561
joanssinjan <einsteinisbae@gmail.com>
2020-03-16 18:55:14 +0000

    r2 vector fit

--- vector_over_det_fit.py
+++ vector_over_det_fit.py
@@ -6,7 +6,7 @@ import os
 import matplotlib.pyplot as plt
 import scipy.optimize as spo
 
-def vector_linalg_lstsq(file_path, inst, day):
+def vector_linalg_lstsq(file_path):
     df = pd.read_csv(file_path)
     df = df.iloc[:-1] #exclude probe 12
 
@@ -19,7 +19,7 @@ def vector_linalg_lstsq(file_path, inst, day):
     b = np.zeros((33,1))
     j = 0
     for i in range(0,11):
-        print(j)
+        #print(j)
         df_tmp = df.iloc[i]
         b[j][0] = df_tmp['X.slope_cur']
         b[j+1][0] = df_tmp['Y.slope_cur']
@@ -27,9 +27,9 @@ def vector_linalg_lstsq(file_path, inst, day):
        
         loc = probe_loc_list[i]
         x, y, z = loc[0], loc[1], loc[2]
-        print('x,y,z = ', x, ',', y, ',', z)
+        #print('x,y,z = ', x, ',', y, ',', z)
         r = probe_dist_list[i]
-        print('r = ', r)
+        #print('r = ', r)
         
         a[j][0] = (3*(x**2)/r**5)-(1/r**3)
         a[j][1] = 3*y*z/r**5
@@ -46,14 +46,15 @@ def vector_linalg_lstsq(file_path, inst, day):
     a = 10**(-7)*a
     b = 10**-9*b #get in units of Tesla
     m, rss, rank, s = np.linalg.lstsq(a,b, rcond=None)
-    print(m)
-    print(rss)
-    print(rank)
-    print(s)
+    #print(m)
+    #print(rss)
+    #print(rank)
+    #print(s)
     mean_bx = np.mean([b for b in b[::3,0]])
     mean_by = np.mean([b for b in b[1::3,0]])
     mean_bz = np.mean([b for b in b[2::3,0]])
-    print(mean_bx, mean_by, mean_bz)
+    mean_b = np.mean(b)
+    #print(mean_bx, mean_by, mean_bz)
 
     tss_x = sum([(b_i - mean_bx)**2 for b_i in b[::3,0]])
     tss_y = sum([(b_i - mean_by)**2 for b_i in b[1::3,0]])
@@ -61,23 +62,33 @@ def vector_linalg_lstsq(file_path, inst, day):
     r_2 = 1 - rss / np.sqrt(tss_x**2+tss_y**2 + tss_z**2)#np.sum((b**2))
     N = np.identity(33) - 1/33 * np.ones((33,33))
     test_tss = b.T @ N @ b 
-    print('r_2 = ', r_2)
-    print(1-rss/np.sum((b**2)))
-    print(1-rss/test_tss)
+    #print('r_2 = ', r_2)
+    r2_conv = 1-rss/np.sum(b**2)
+    #print(round(r2_conv[0],3))
+    r_2_new  = 1-rss/test_tss
+    #print(r_2_new[0][0])
     r_2_adj = 1 - ((1-r_2)*(10)/6) #3 or 1 independent variable? I think 3
     #print(r_2_adj)
+    tss = [(b_i-mean_b)**2 for b_i in b]
+    r_2_test = 1 - rss/sum(tss)
+    r_2_test = 1 - rss/(33*b.var())
+    print(round(r_2_test[0],3))
     return m, rss
 
 if __name__ == "__main__":
     windows = True
-    inst = 'PHI'
-    day = 1
+    #inst = 'PHI'
+    day = 2
+    instru_list = ['EUI','METIS','PHI','SWA','SoloHI','STIX','SPICE','EPD']
+    
+    
 
-    if windows:
-        file_path = f'.\\Results\\Gradient_dicts\\Day_{day}\\1hz_noorigin\\cur\\{inst}_vect_dict_NOORIGIN_Day{day}_curve_fit.csv'
-    else:
-        file_path = os.path.expanduser(f'./Results/Gradient_dicts/Day_{day}/1hz_noorigin/cur/{inst}_vect_dict_NOORIGIN_Day{day}_curve_fit.csv')
-
-    vector_linalg_lstsq(file_path, inst, day)
+    for inst in instru_list:
+        #print(inst)
+        if windows:
+            file_path = f'.\\Results\\Gradient_dicts\\Day_{day}\\1hz_noorigin\\cur\\{inst}_vect_dict_NOORIGIN_Day{day}_curve_fit.csv'
+        else:
+            file_path = os.path.expanduser(f'./Results/Gradient_dicts/Day_{day}/1hz_noorigin/cur/{inst}_vect_dict_NOORIGIN_Day{day}_curve_fit.csv')
+        vector_linalg_lstsq(file_path)
 
 
