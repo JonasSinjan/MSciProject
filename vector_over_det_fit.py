0d9e3ae96e05bc2241c608d822cef8d5f728951b
joanssinjan <einsteinisbae@gmail.com>
2020-04-05 15:06:35 +0100

    manual predict values with moments

--- vector_over_det_fit.py
+++ vector_over_det_fit.py
@@ -6,7 +6,7 @@ import os
 import matplotlib.pyplot as plt
 import scipy.optimize as spo
 
-def vector_linalg_lstsq(file_path, probes, max_current):
+def vector_linalg_lstsq(file_path, probes, sig_axes, max_current):
     df = pd.read_csv(file_path)
     df = df.iloc[:-1] #exclude probe 12
 
@@ -17,15 +17,17 @@ def vector_linalg_lstsq(file_path, probes, max_current):
 
     #a = np.zeros((33,3))
     #b = np.zeros((33,1))
-    a = np.zeros((len(probes)*3, 3))
-    b = np.zeros((len(probes)*3, 1))
+    # a = np.zeros((7, 3))
+    # b = np.zeros((7, 1))
+    a = np.zeros((11, 3))
+    b = np.zeros((11, 1))
     j = 0
     for i in probes:
         #print(j)
         df_tmp = df.iloc[i]
-        b[j][0] = df_tmp['X.slope_cur']*max_current
-        b[j+1][0] = df_tmp['Y.slope_cur']*max_current
-        b[j+2][0] = df_tmp['Z.slope_cur']*max_current
+        #b[j][0] = df_tmp['X.slope_cur']*max_current
+        #b[j+1][0] = df_tmp['Y.slope_cur']*max_current
+        #b[j+2][0] = df_tmp['Z.slope_cur']*max_current
        
         loc = probe_loc_list[i]
         if i == 8:
@@ -38,18 +40,29 @@ def vector_linalg_lstsq(file_path, probes, max_current):
             r = probe_dist_list[i+1]
         #print('r = ', r)
         
-        a[j][0] = (3*(x**2)/r**5)-(1/r**3)
-        a[j][1] = 3*y*z/r**5
-        a[j][2] = 3*z*x/r**5
-        a[j+1][0] = 3*x*y/r**5
-        a[j+1][1] = 3*(y**2)/r**5-1/r**3
-        a[j+1][2] = 3*z*y/r**5
-        a[j+2][0] = 3*x*z/r**5
-        a[j+2][1] = 3*y*z/r**5
-        a[j+2][2] = (3*(z**2)/r**5)-(1/r**3)
+        if 'X' in sig_axes[i]:
+            b[j][0] = df_tmp['X.slope_cur']*max_current
+            a[j][0] = (3*(x**2)/r**5)-(1/r**3)
+            a[j][1] = 3*y*z/r**5
+            a[j][2] = 3*z*x/r**5
+            j += 1
+
+        if 'Y' in sig_axes[i]:
+            b[j][0] = df_tmp['Y.slope_cur']*max_current
+            a[j][0] = 3*x*y/r**5
+            a[j][1] = 3*(y**2)/r**5-1/r**3
+            a[j][2] = 3*z*y/r**5
+            j += 1
+
+        if 'Z' in sig_axes[i]:
+            b[j][0] = df_tmp['Z.slope_cur']*max_current
+            a[j][0] = 3*x*z/r**5
+            a[j][1] = 3*y*z/r**5
+            a[j][2] = (3*(z**2)/r**5)-(1/r**3)
+            j +=1 
         #if i == 9:
         #    print(a @ np.array([[14.1e-3],[-7.7e-3], [0.9e-3]]))
-        j += 3
+        #j += 3
         
     a = 10**(-7)*a
     b = 10**-9*b #get in units of Tesla
@@ -86,18 +99,26 @@ def vector_linalg_lstsq(file_path, probes, max_current):
 if __name__ == "__main__":
     windows = True
     #inst = 'PHI'
-    day = 2
-    instru_list = ['EUI']#,'PHI','SWA','SoloHI','STIX','SPICE','EPD']
+    day = 1
+    instru_list = ['METIS']#,'PHI','SWA','SoloHI','STIX','SPICE','EPD']
     if 'EUI' in instru_list:
         max_current = 0.8
     if 'METIS' in instru_list:
         max_current = 0.95
 
-    #sig_probes = [0,1,2,4,5,6,7] #d2 metis
+    
     #sig_probes = [0,1,2,6,7,8] #d1 metis
+    sig_probes = [0,1,2,4,5,6,7] #d2 metis
 
     #sig_probes = [4,5,6,8] #d1 eui
-    sig_probes = [0,3,5,6,8] #d2 eui
+    #sig_probes = [0,3,5,6,8] #d2 eui
+
+    #sig_axes = {0: ['X','Z'], 1: ['X','Y'], 2: ['Y','Z'], 6: ['X','Y'], 7:['X','Y'], 8: ['X']} #d1 metis
+    sig_axes = {0: ['X','Y'], 1: ['Y'], 2: ['X'], 4: ['Y','Z'], 5: ['X','Y'], 6: ['X','Y'], 7: ['Y']} #d2 metis
+    
+
+    #sig_axes = {4: ['X'], 5: ['Y','Z'], 6: ['X','Y'], 8: ['Y']} #d1 eui
+    #sig_axes = {0: ['X','Y'], 3: ['Z'], 5: ['Z'], 6: ['Y'], 8: ['Y', 'Z']} #d2 eui
 
     for inst in instru_list:
         #print(inst)
@@ -107,6 +128,36 @@ if __name__ == "__main__":
         else:
             #file_path = os.path.expanduser(f'./Results/Gradient_dicts/Day_{day}/1hz_noorigin/cur/{inst}_vect_dict_NOORIGIN_Day{day}_curve_fit.csv')
             file_path = os.path.expanduser(f'./Results/Gradient_dicts/newdI_dicts/Day_{day}/cur/{inst}_vect_dict_NOORIGIN_Day{day}_curve_fit.csv')
-        vector_linalg_lstsq(file_path, sig_probes, max_current)
+        #vector_linalg_lstsq(file_path, sig_probes, sig_axes, max_current)
 
+
+    x = 0.75
+    y = -1.3
+    z = -1.76
+    r = 2.328
+
+    x = 0.2
+    y = -1.05
+    z = -4.015
+    r = 4.155
+    a = np.zeros((3,3))
+    j=0
+    a[j][0] = (3*(x**2)/r**5)-(1/r**3)
+    a[j][1] = 3*y*z/r**5
+    a[j][2] = 3*z*x/r**5
+    a[j+1][0] = 3*x*y/r**5
+    a[j+1][1] = 3*(y**2)/r**5-1/r**3
+    a[j+1][2] = 3*z*y/r**5
+    a[j+2][0] = 3*x*z/r**5
+    a[j+2][1] = 3*y*z/r**5
+    a[j+2][2] = (3*(z**2)/r**5)-(1/r**3)
+
+    x = np.zeros((3,1))
+    x[0][0] = 8.5
+    x[1][0] = -6.7
+    x[2][0] = -18.1
+
+    b = 10**(-7)*a @ x/1000
+    mag = np.sqrt(b[0][0]**2 + b[1][0]**2 + b[2][0]**2)
+    print(mag/10**(-9))
 
