1823e3bac3f191461d4e3e44b02ad18e8fc09806
joanssinjan <einsteinisbae@gmail.com>
2020-05-01 14:00:59 +0100

    update error compar graphs

--- read_dBdI.py
+++ read_dBdI.py
@@ -166,15 +166,17 @@ def plot_new_curve(windows, day, instruments, probes, sample):
             errorbar_list = [probe_x_tmp, probe_x_tmp_err, probe_y_tmp, probe_y_tmp_err, probe_z_tmp, probe_z_tmp_err]
             j = 0
             axis = ['X','Y','Z']
+            plt.rcParams.update({'font.size': 12})
             for i in [0,1,2]:
                 correlation_matrix = np.corrcoef(xdata, errorbar_list[j])
                 correlation_xy = correlation_matrix[0,1]
                 r_squared = correlation_xy**2
-                plt.plot(xdata, params_list[i][0]*xdata + params_list[i][1], '-', color = u'#1f77b4', label=f'R2 = {r_squared}')
-                plt.errorbar(xdata, errorbar_list[j], yerr = errorbar_list[j+1], fmt = 'o', color = u'#1f77b4', capsize = cap_size, capthick = cap_thick, ecolor = error_colour, elinewidth = eline_width, markeredgecolor = "white", markersize = ms, label = f'{axis[i]} grad: {round(params_list[i][0],2)} $\pm$ {round(perr_list[i][0],2)} nT/A')#, int: {round(params_x[1],2)} ± {round(perr_x[1],2)}')
+                plt.figure(figsize=(8.0,6.0))
+                plt.errorbar(xdata, errorbar_list[j], yerr = errorbar_list[j+1], fmt = 'o', color = u'#ff7f0e', capsize = cap_size, capthick = cap_thick, ecolor = error_colour, elinewidth = eline_width, markeredgecolor = "white", markersize = ms, label = f'{axis[i]} gradient: {round(params_list[i][0],2)} $\pm$ {round(perr_list[i][0],2)} nT/A')#, int: {round(params_x[1],2)} ± {round(perr_x[1],2)}')
+                plt.plot(xdata, params_list[i][0]*xdata + params_list[i][1], '-', color = u'#ff7f0e', label=f'$R^{2}$ = {round(r_squared,2)}')
                 j += 2
                 plt.legend(loc="best")
-                plt.title(f'Day {day} - {inst} - Probe {probe} {axis[i]} - {sample}Hz')
+                #plt.title(f'Day {day} - {inst} - Probe {probe} {axis[i]} - {sample}Hz')
                 plt.xlabel('dI [A]')
                 plt.ylabel('dB [nT]')
                 plt.show()
@@ -194,8 +196,8 @@ def plot_new_curve(windows, day, instruments, probes, sample):
 if __name__ == "__main__":
     windows = True
     day = 2
-    instruments = ['SoloHI']#, 'METIS', 'PHI', 'SWA', 'SoloHI', 'STIX', 'SPICE', 'EPD']
-    probes = range(1,12)
+    instruments = ['EUI']#, 'METIS', 'PHI', 'SWA', 'SoloHI', 'STIX', 'SPICE', 'EPD']
+    probes = [5]#range(1,12)
     sample_rate = 1
 
     #plot_old_errs_with_lin(windows,day,instruments,probes,sample_rate)
